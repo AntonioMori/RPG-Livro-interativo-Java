958a7322b82e8aaf6db9987ffe67631d6a772082
Antonio Mori <camargomoritrancoso@hotmail.com>
2023-11-30 15:27:29 -0300

    Aperfeiçoamentos metodos(escolha e verificaNome)
    Adicionados os verificadores de respostas válidas para o input do nome em seu capitulo referente, e relacionado as escolhas, agora se o input de uma escolha for diferente de um inteiro não da erro, apenas repete a pergunta até que seja informado um inteiro e que seja um referente a uma das opções de resposta

--- src/principal/capitulo.java
+++ src/principal/capitulo.java
@@ -1,15 +1,17 @@
 package principal;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.InputMismatchException;
+
 
 
 
 public class capitulo{
     
     private String texto;
-    private String pergunta;
+    private String pergunta = dados.escolhas.escolhaPergunta;
     private personagem personagem;
-    
     private int dano;
     private int estamina;
     private boolean diferencial;
@@ -19,17 +21,13 @@ public class capitulo{
     static Scanner input = new Scanner(System.in);
 
     public capitulo(String texto) {
-        
         this.texto = texto;
-        
-        pergunta = "Escolha uma opção: ";
     }
 
     //construtor capitulo final
     public capitulo(String texto,personagem personagem,int dano, int estamina){
         this.texto =texto;
         this.personagem = personagem;
-        
         this.dano= dano;
         this.estamina=estamina;
 
@@ -47,13 +45,14 @@ public class capitulo{
         
         System.out.println(texto);//mostrar texto
 
-        if (diferencial==true) {
+        if (diferencial==true) { // se for um capitulo diferencial, ele precisa de um nome
                 
             System.out.println("Digite seu nome: ");
-            
-            
             String name = input.next();
-            this.personagem.setName(name);
+            String verifyedName = verificaNome(name);
+            
+
+            this.personagem.setName(verifyedName);
             System.out.println("\nAo ler o seu nome '" + this.personagem.getNome() + "' você se lembra de como foi parar lá, mas percebe que um"+ 
             " carangueijo gigante está vindo em sua direção, o que você faz?\n");
             diferencial =false;
@@ -61,13 +60,14 @@ public class capitulo{
         }
         if (arrayEscolhas != null) {
             for (int i = 0; i < arrayEscolhas.length; i++) {
-            String escolhaPrint = arrayEscolhas[i].getTexto();
-            System.out.println(i+" - "+escolhaPrint); 
+             
+            System.out.println(i+" - "+ arrayEscolhas[i].getTexto()); 
             
             }
             System.out.println("\n"+pergunta);
         
-        }else{
+        }else{ // se nao houver um array de escolhas entao é um capitulo final logo, 
+            //é visto as consequencias das suas escolhas
             this.personagem.setVida(this.dano);
             this.personagem.setEstamina(this.estamina);
         }
@@ -77,7 +77,7 @@ public class capitulo{
         
     }
 
-    public boolean buscaIndice(int indice)
+    private boolean buscaIndice(int indice)
     {
         
         for (int i = 0; i < arrayEscolhas.length; i++) 
@@ -92,30 +92,43 @@ public class capitulo{
         
     }
 
-    public void escolher(){ // ele precisa
-        
-        int resposta = input.nextInt(); //entrada do usuario
-        while (buscaIndice(resposta)==false) {
-            System.out.println("\nOpção inválida, por favor digite novamente:");
-            resposta = input.nextInt();
-        }       
+    private void escolher() {
+        int resposta;
 
-            for (int i = 0; i < arrayEscolhas.length; i++) {
-                if (resposta == i) {
-                    
-                    capitulo proximo = arrayEscolhas[i].getProximo();
-                    proximo.executar();
-                    
+        do {
+            try {
+                
+                resposta = input.nextInt();
+
+                if (buscaIndice(resposta) != true) {
+                    System.out.println("Opção inválida, por favor digite novamente.");
                 }
+            } catch (InputMismatchException e) {
+                // Limpa o buffer do scanner em caso de entrada inválida
+                input.nextLine();
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                resposta = -1; // Define resposta como -1 para continuar no loop
             }
+        } while (!buscaIndice(resposta));
 
-    } 
+
+        
+        // Executa a escolha válida
+        for (int i = 0; i < arrayEscolhas.length; i++) {
+            if (resposta == i) {
+                capitulo proximo = arrayEscolhas[i].getProximo();
+                proximo.executar();
+            }
+        }
+    }
+
+   
     
 
     public void executar(){
         mostrar();
         escolher();
-        executar();
+        
         
     }
 
@@ -127,5 +140,25 @@ public class capitulo{
         this.arrayEscolhas = arrayEscolhas;
     }
 
+    private String verificaNome(String name){
+        int n = 1;
+        while (isValidName(name)==false) {
+                if (n>2) {
+                    System.out.println("\nVocê se esforçou tanto para tentar se lembrar do seu nome que voce desmaiou.");
+                    this.personagem.setVida(-100);
+                }
+                else{
+                    System.out.println("\n Nome inválido, por favor digite novamente: ");
+                    name = input.next();
+                    n++;
+                }
+            }
+            return name;
+    }
 
+
+    //verifica se o nome é valido
+    private boolean isValidName(String name) {
+        return name != null && Pattern.matches("^[a-zA-Z\\s]+$", name) && name.length() <= 36 && name.length() >= 4;
+    }
 }
